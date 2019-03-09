915cc3e36152b6d938b6f54e527fee1e42bccb1b
YangYun <1186584388@qq.com>
2019-03-09 14:58:32 +0800

    Update Pix2Pix.py

--- 3-Advanced_Models/Pix2Pix.py
+++ 3-Advanced_Models/Pix2Pix.py
@@ -24,9 +24,7 @@ as described in Image-to-Image Translation with Conditional Adversarial Networks
 
 _URL = 'https://people.eecs.berkeley.edu/~tinghuiz/projects/pix2pix/datasets/facades.tar.gz'
 
-path_to_zip = tf.keras.utils.get_file('facades.tar.gz',
-                                      origin=_URL,
-                                      extract=True)
+path_to_zip = tf.keras.utils.get_file('facades.tar.gz', origin=_URL, extract=True)
 
 PATH = os.path.join(os.path.dirname(path_to_zip), 'facades/')
 
