3d5b0f2c5fe36107dafff99b90c88bf5d693ef14
四一 <1186584388@qq.com>
2019-07-18 20:44:28 +0800

    config

--- 4-Object_Detection/YOLOV3/core/config.py
+++ 4-Object_Detection/YOLOV3/core/config.py
@@ -28,7 +28,6 @@ __C.YOLO.ANCHORS                = "./data/anchors/basline_anchors.txt"
 __C.YOLO.STRIDES                = [8, 16, 32]
 __C.YOLO.ANCHOR_PER_SCALE       = 3
 __C.YOLO.IOU_LOSS_THRESH        = 0.5
-__C.YOLO.UPSAMPLE_METHOD        = "resize"
 
 # Train options
 __C.TRAIN                       = edict()
@@ -40,7 +39,7 @@ __C.TRAIN.INPUT_SIZE            = [416]
 __C.TRAIN.DATA_AUG              = True
 __C.TRAIN.LEARN_RATE_INIT       = 1e-4
 __C.TRAIN.LEARN_RATE_END        = 1e-6
-__C.TRAIN.WARMUP_EPOCHS         = 2
+__C.TRAIN.WARMUP_EPOCHS         = 4
 __C.TRAIN.EPOCHS                = 20
 
 
