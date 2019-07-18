638e2c15324a6ab0b07e60959f851885a1ead7b4
四一 <1186584388@qq.com>
2019-07-18 11:54:16 +0800

    Update config.py

--- 4-Object_Detection/YOLOV3/core/config.py
+++ 4-Object_Detection/YOLOV3/core/config.py
@@ -23,7 +23,7 @@ cfg                             = __C
 __C.YOLO                        = edict()
 
 # Set the class name
-__C.YOLO.CLASSES                = "./data/classes/yymnist.names"
+__C.YOLO.CLASSES                = "./data/classes/coco.names"
 __C.YOLO.ANCHORS                = "./data/anchors/basline_anchors.txt"
 __C.YOLO.STRIDES                = [8, 16, 32]
 __C.YOLO.ANCHOR_PER_SCALE       = 3
@@ -48,20 +48,16 @@ __C.TRAIN.EPOCHS                = 20
 # TEST options
 __C.TEST                        = edict()
 
-__C.TEST.ANNOT_PATH             = "./data/dataset/voc_test.txt"
+__C.TEST.ANNOT_PATH             = "./data/dataset/yymnist_test.txt"
 __C.TEST.BATCH_SIZE             = 2
 __C.TEST.INPUT_SIZE             = 544
 __C.TEST.DATA_AUG               = False
 __C.TEST.WRITE_IMAGE            = True
 __C.TEST.WRITE_IMAGE_PATH       = "./data/detection/"
 __C.TEST.WRITE_IMAGE_SHOW_LABEL = False
-__C.TEST.WEIGHT_FILE            = "./checkpoint/yolov3_test_loss=9.2099.ckpt-5"
+__C.TEST.WEIGHT_FILE            = "./checkpoint/yolov3.ckpt"
 __C.TEST.SHOW_LABEL             = False
 __C.TEST.SCORE_THRESHOLD        = 0.3
 __C.TEST.IOU_THRESHOLD          = 0.45
-
-
-
-
 
 
