40db78e8681ad0642d987a943ada3eec4cc2c76a
YunYang1994 <YunYang1994@github.com>
2019-10-17 11:38:13 +0800

    FCN train

--- 5-Image_Segmentation/FCN/parser_voc.py
+++ 5-Image_Segmentation/FCN/parser_voc.py
@@ -12,23 +12,27 @@
 #================================================================
 
 import os
+import argparse
 import numpy as np
 from config import colormap
 from scipy import misc
-
-VOC_path = "/home/yang/dataset/VOC"
 
 if not os.path.exists("./data"): os.mkdir("./data")
 if not os.path.exists("./data/train_labels"): os.mkdir("./data/train_labels")
 if not os.path.exists("./data/test_labels"): os.mkdir("./data/test_labels")
 
+parser = argparse.ArgumentParser()
+parser.add_argument("--voc_path", type=str, default="/home/yang/dataset/VOC")
+flags = parser.parse_args()
+if not os.path.exists(flags.voc_path): # "/home/yang/dataset/VOC"
+    raise ValueError("Path: %s does not exist" %flags.voc_path)
 
 for mode in ["train", "test"]:
     image_write = open(os.path.join(os.getcwd(), "data/%s_image.txt" %mode), "w")
     for year in [2007, 2012]:
         if mode == "test" and year == 2012: continue
-        train_label_folder = os.path.join(VOC_path, "%s/VOCdevkit/VOC%d/SegmentationClass" %(mode, year))
-        train_image_folder = os.path.join(VOC_path, "%s/VOCdevkit/VOC%d/JPEGImages" %(mode, year))
+        train_label_folder = os.path.join(flags.voc_path, "%s/VOCdevkit/VOC%d/SegmentationClass" %(mode, year))
+        train_image_folder = os.path.join(flags.voc_path, "%s/VOCdevkit/VOC%d/JPEGImages" %(mode, year))
         train_label_images = os.listdir(train_label_folder)
 
         for train_label_image in train_label_images:
@@ -51,7 +55,4 @@ for mode in ["train", "test"]:
                         cls_idx = 0
                     write_line.append(str(cls_idx))
                 write_label.writelines(" ".join(write_line) + "\n")
-
-
-
 
