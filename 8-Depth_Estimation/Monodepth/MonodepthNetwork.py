488d01fa5572061fb8b55d19cb9a3b144da7054a
YunYang1994 <YunYang1994@github.com>
2019-11-03 00:43:49 +0800

    Dispnet

---
+++ 8-Depth_Estimation/Monodepth/MonodepthNetwork.py
@@ -0,0 +1,166 @@
+#! /usr/bin/env python
+# coding=utf-8
+#================================================================
+#   Copyright (C) 2019 * Ltd. All rights reserved.
+#
+#   Editor      : VIM
+#   File name   : MonodepthNetwork.py
+#   Author      : YunYang1994
+#   Created date: 2019-11-01 23:57:18
+#   Description :
+#
+#================================================================
+
+import tensorflow as tf
+from utils import upsample_nn
+
+class Bottleneck(tf.keras.Model):
+    expansion = 4
+
+    def __init__(self, in_channels, out_channels, strides=1):
+        super(Bottleneck, self).__init__()
+
+        self.conv1 = tf.keras.layers.Conv2D(out_channels, 1, 1, use_bias=False)
+        self.bn1 = tf.keras.layers.BatchNormalization()
+        self.conv2 = tf.keras.layers.Conv2D(out_channels, 3, strides, padding="same", use_bias=False)
+        self.bn2 = tf.keras.layers.BatchNormalization()
+        self.conv3 = tf.keras.layers.Conv2D(out_channels*self.expansion, 1, 1, use_bias=False)
+        self.bn3 = tf.keras.layers.BatchNormalization()
+
+        if strides != 1 or in_channels != self.expansion * out_channels:
+            self.shortcut = tf.keras.Sequential([
+                    tf.keras.layers.Conv2D(self.expansion*out_channels, kernel_size=1,
+                                           strides=strides, use_bias=False),
+                    tf.keras.layers.BatchNormalization()]
+                    )
+        else:
+            self.shortcut = lambda x: x
+
+    def call(self, x, training=False):
+        out = tf.nn.relu(self.bn1(self.conv1(x), training))
+        out = tf.nn.relu(self.bn2(self.conv2(out), training))
+        out = self.bn3(self.conv3(out), training)
+        out += self.shortcut(x)
+        return tf.nn.relu(out)
+
+class resnet_encoder(tf.keras.Model):
+    """ take resnet50 as backbone"""
+    def __init__(self):
+        super(resnet_encoder, self).__init__()
+
+        self.in_channels = 64
+        # Encode layers
+        self.conv1 = tf.keras.layers.Conv2D(64, 7, 2, padding="same", use_bias=False)
+        self.bn1 = tf.keras.layers.BatchNormalization()
+        self.pad = tf.keras.layers.ZeroPadding2D(padding=1)
+        self.pool1 = tf.keras.layers.MaxPool2D(3, 2, padding="valid") # H/4  -   64D
+        self.layer1 = self._make_layer(Bottleneck,  64, 3, 2)         # H/8  -  256D
+        self.layer2 = self._make_layer(Bottleneck, 128, 4, 2)         # H/16 -  512D
+        self.layer3 = self._make_layer(Bottleneck, 256, 6, 2)         # H/32 - 1024D
+        self.layer4 = self._make_layer(Bottleneck, 512, 3, 2)         # H/64 - 2048D
+
+    def _make_layer(self, block, out_channels, num_blocks, stride):
+        strides = [stride] + [1] * (num_blocks - 1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_channels, out_channels, stride))
+            self.in_channels = out_channels * block.expansion
+        return tf.keras.Sequential(layers)
+
+    def call(self, x, training=False):
+        out = tf.nn.relu(self.bn1(self.conv1(x), training))
+        skip1 = out
+        out = self.pool1(self.pad(out))
+        skip2 = out
+        out = self.layer1(out)
+        skip3 = out
+        out = self.layer2(out)
+        skip4 = out
+        out = self.layer3(out)
+        skip5 = out
+        out = self.layer4(out)
+        return skip1, skip2, skip3, skip4, skip5, out
+
+
+class upsample_conv(tf.keras.Model):
+    def __init__(self, filters, kernel_size, scale):
+        super(upsample_conv, self).__init__()
+        self.scale = scale
+        self.conv = tf.keras.Sequential([tf.keras.layers.ZeroPadding2D(1),
+                tf.keras.layers.Conv2D(filters, kernel_size, strides=1, padding="valid", activation="relu")]
+                )
+
+    def call(self, x, training=False):
+        out = upsample_nn(x, self.scale)
+        out = self.conv(out)
+        return out
+
+class depth_decoder(tf.keras.Model):
+    def __init__(self):
+        super(depth_decoder, self).__init__()
+        self.out_channels = [16, 32, 64, 128, 256, 512]
+        self.upsample_conv_layers = [upsample_conv(x, 3, 2) for x in self.out_channels]
+        self.conv_layers = [self._conv3x3(x, 3, 1, "relu") for x in self.out_channels]
+        self.disp_layers = [self._conv3x3(2, 3, 1, "sigmoid") for _ in range(4)]
+
+    def _conv3x3(self, out_channels, kernel_size, strides, activation_fn):
+        return tf.keras.Sequential([
+                tf.keras.layers.ZeroPadding2D(1),
+                tf.keras.layers.Conv2D(out_channels, kernel_size, strides,
+                    padding="valid", activation=activation_fn)]
+                )
+
+
+    def call(self, input_features, training=False):
+        skip1, skip2, skip3, skip4, skip5, out = input_features
+
+        upconv5 = self.upsample_conv_layers[5](out) # H/32 - 512D
+        print(upconv5.shape)
+        concat5 = tf.concat([upconv5, skip5], axis=3)
+        iconv5  = self.conv_layers[5](concat5)
+
+        upconv4 = self.upsample_conv_layers[4](iconv5) # H/16 - 256D
+        print(upconv4.shape)
+        concat4 = tf.concat([upconv4, skip4], axis=3)
+        iconv4  = self.conv_layers[4](concat4)
+
+        upconv3 = self.upsample_conv_layers[3](iconv4)
+        concat3 = tf.concat([upconv3, skip3], axis=3)
+        iconv3  = self.conv_layers[3](concat3)
+        self.disp3 = 0.3 * self.disp_layers[3](iconv3) # H/8 - 2D
+        print(self.disp3.shape)
+        up_disp3 = upsample_nn(self.disp3, 2)
+
+        upconv2 = self.upsample_conv_layers[2](iconv3)
+        concat2 = tf.concat([upconv2, skip2, up_disp3], axis=3)
+        iconv2  = self.conv_layers[2](concat2)
+        self.disp2 = 0.3 * self.disp_layers[2](iconv2) # H/4 - 2D
+        print(self.disp2.shape)
+        up_disp2 = upsample_nn(self.disp2, 2)
+
+        upconv1 = self.upsample_conv_layers[1](iconv2)
+        concat1 = tf.concat([upconv1, skip1, up_disp2], axis=3)
+        iconv1  = self.conv_layers[1](concat1)
+        self.disp1 = 0.3 * self.disp_layers[1](iconv1) # H/2 - 2D
+        print(self.disp1.shape)
+        up_disp1 = upsample_nn(self.disp1, 2)
+
+        upconv0 = self.upsample_conv_layers[0](iconv1)
+        concat0 = tf.concat([upconv0, up_disp1], axis=3)
+        iconv0  = self.conv_layers[0](concat0)
+        self.disp0 = 0.3 * self.disp_layers[0](iconv0) # H - 2D
+        print(self.disp0.shape)
+        return self.disp0, self.disp1, self.disp2, self.disp3
+
+class MonodepthNetwork(tf.keras.Model):
+    def __init__(self):
+        super(MonodepthNetwork, self).__init__()
+        self.encoder = resnet_encoder()
+        self.decoder = depth_decoder()
+
+    def call(self, x, training=False):
+        out = self.encoder(x)
+        out = self.decoder(out)
+        return out
+
+
---
+++ 8-Depth_Estimation/Monodepth/loss.py
@@ -0,0 +1,107 @@
+#! /usr/bin/env python
+# coding=utf-8
+#================================================================
+#   Copyright (C) 2019 * Ltd. All rights reserved.
+#
+#   Editor      : VIM
+#   File name   : loss.py
+#   Author      : YunYang1994
+#   Created date: 2019-11-02 10:48:03
+#   Description :
+#
+#================================================================
+
+import tensorflow as tf
+
+from MonodepthNetwork import MonodepthNetwork
+from utils import split_lr_disp, bilinear_sampler_1d_h, scale_pyramid, generate_left_image, generate_right_image
+
+def SSIM(x, y):
+    C1 = 0.01 ** 2
+    C2 = 0.03 ** 2
+
+    mu_x = tf.nn.avg_pool2d(x, 3, 1, 'VALID')
+    mu_y = tf.nn.avg_pool2d(y, 3, 1, 'VALID')
+
+    sigma_x  = tf.nn.avg_pool2d(x ** 2, 3, 1, 'VALID') - mu_x ** 2
+    sigma_y  = tf.nn.avg_pool2d(y ** 2, 3, 1, 'VALID') - mu_y ** 2
+    sigma_xy = tf.nn.avg_pool2d(x * y , 3, 1, 'VALID') - mu_x * mu_y
+
+    SSIM_n = (2 * mu_x * mu_y + C1) * (2 * sigma_xy + C2)
+    SSIM_d = (mu_x ** 2 + mu_y ** 2 + C1) * (sigma_x + sigma_y + C2)
+    SSIM = SSIM_n / SSIM_d
+    return tf.clip_by_value((1 - SSIM) / 2, 0, 1)
+
+def get_disparity_smoothness(disp, pyramid):
+    gradient_x = lambda img: img[:,:,:-1,:] - img[:,:,1:,:]
+    gradient_y = lambda img: img[:,:-1,:,:] - img[:,1:,:,:]
+
+    disp_gradients_x = [gradient_x(d) for d in disp]
+    disp_gradients_y = [gradient_y(d) for d in disp]
+
+    image_gradients_x = [gradient_x(img) for img in pyramid]
+    image_gradients_y = [gradient_y(img) for img in pyramid]
+
+    weights_x = [tf.exp(-tf.reduce_mean(tf.abs(g), 3, keepdims=True)) for g in image_gradients_x]
+    weights_y = [tf.exp(-tf.reduce_mean(tf.abs(g), 3, keepdims=True)) for g in image_gradients_y]
+
+    smoothness_x = [disp_gradients_x[i] * weights_x[i] for i in range(4)]
+    smoothness_y = [disp_gradients_y[i] * weights_y[i] for i in range(4)]
+    return smoothness_x + smoothness_y
+
+model = MonodepthNetwork()
+lr_disp = model(left_image)
+left_image = tf.ones(shape=[1, 256, 512, 3])
+right_image = tf.ones(shape=[1, 256, 512, 3]) * 3
+
+
+true_left_pyramid  = scale_pyramid(left_image, 4)
+true_right_pyramid = scale_pyramid(right_image, 4)
+
+left_disp, right_disp = split_lr_disp(lr_disp)
+
+# generate images with disparity
+pred_left_pyramid  = [generate_left_image(true_right_pyramid[i], left_disp[i]) for i in range(4)]
+pred_right_pyramid = [generate_right_image(true_left_pyramid[i], right_disp[i]) for i in range(4)]
+# lr consitency
+
+right_to_left_disp= [generate_left_image(right_disp[i], left_disp[i]) for i in range(4)]
+left_to_right_disp= [generate_right_image(left_disp[i], right_disp[i]) for i in range(4)]
+# disparity smoothness
+
+disp_left_smoothness = get_disparity_smoothness(left_disp, true_left_pyramid)
+disp_right_smoothness = get_disparity_smoothness(right_disp, true_right_pyramid)
+# IMAGE RECONSTRUCTION
+
+# L1 loss
+l1_left = [tf.abs(pred_left_pyramid[i]-true_left_pyramid[i]) for i in range(4)]
+l1_reconstruction_loss_left = [tf.reduce_mean(l) for l in l1_left]
+l1_right = [tf.abs(pred_right_pyramid[i]-true_right_pyramid[i]) for i in range(4)]
+l1_reconstruction_loss_right = [tf.reduce_mean(l) for l in l1_right]
+
+# SSIM loss
+ssim_left = [SSIM(pred_left_pyramid[i], true_left_pyramid[i]) for i in range(4)]
+ssim_loss_left = [tf.reduce_mean(s) for s in ssim_left]
+ssim_right = [SSIM(pred_right_pyramid[i], true_right_pyramid[i]) for i in range(4)]
+ssim_loss_right = [tf.reduce_mean(s) for s in ssim_right]
+
+# WEIGTHED SUM
+alpha = 0.3
+image_loss_right =[alpha*ssim_loss_right[i] + (1-alpha) * l1_reconstruction_loss_right[i] for i in range(4)]
+image_loss_left  =[alpha*ssim_loss_left[i] + (1-alpha) * l1_reconstruction_loss_left[i] for i in range(4)]
+image_loss = tf.add_n(image_loss_left + image_loss_right)
+
+# DISPARITY SMOOTHNESS
+disp_left_loss = [tf.reduce_mean(tf.abs(disp_left_smoothness[i])) / 2 ** i for i in range(4)]
+disp_right_loss = [tf.reduce_mean(tf.abs(disp_right_smoothness[i])) / 2 ** i for i in range(4)]
+disp_gradient_loss = tf.add_n(disp_left_loss + disp_right_loss)
+
+# LR CONSISTENCY
+lr_left_loss = [tf.reduce_mean(tf.abs(right_to_left_disp[i] - left_disp[i])) for i in range(4)]
+lr_right_loss = [tf.reduce_mean(tf.abs(left_to_right_disp[i] - right_disp[i])) for i in range(4)]
+lr_loss = tf.add_n(lr_left_loss + lr_right_loss)
+
+# TOTAL LOSS
+total_loss = image_loss + 0.1 * disp_gradient_loss + lr_loss
+
+
