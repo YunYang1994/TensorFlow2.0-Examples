f8d9ddb962e3f02a22fd2d30abd6a6724b509024
WeikangWang <wwang72@vols.utk.edu>
2019-12-14 16:23:03 -0500

    adapter the darknet53 model to a much simpler model to fit the easy data for power system

---
+++ 4-Object_Detection/YOLOV3/core/backbone_fnet.py
@@ -0,0 +1,50 @@
+#! /usr/bin/env python
+# coding=utf-8
+#================================================================
+#   Copyright (C) 2019 * Ltd. All rights reserved.
+#
+#   Editor      : VIM
+#   File name   : backbone.py
+#   Author      : YunYang1994
+#   Created date: 2019-07-11 23:37:51
+#   Description :
+#
+#================================================================
+
+import tensorflow as tf
+import core.common as common
+
+
+def grid_eye_net_18(input_data):
+
+    input_data = common.convolutional(input_data, (3, 3,  3,  32))
+    input_data = common.convolutional(input_data, (3, 3, 32,  64), downsample=True)
+
+    for i in range(1):
+        input_data = common.residual_block(input_data,  64,  32, 64)
+
+    input_data = common.convolutional(input_data, (3, 3,  64, 128), downsample=True)
+
+    for i in range(1):
+        input_data = common.residual_block(input_data, 128,  64, 128)
+
+    input_data = common.convolutional(input_data, (3, 3, 128, 256), downsample=True)
+
+    for i in range(2):
+        input_data = common.residual_block(input_data, 256, 128, 256)
+
+    route_1 = input_data
+    input_data = common.convolutional(input_data, (3, 3, 256, 512), downsample=True)
+
+    for i in range(2):
+        input_data = common.residual_block(input_data, 512, 256, 512)
+
+    route_2 = input_data
+    input_data = common.convolutional(input_data, (3, 3, 512, 1024), downsample=True)
+
+    for i in range(1):
+        input_data = common.residual_block(input_data, 1024, 512, 512)
+
+    return route_1, route_2, input_data
+
+
